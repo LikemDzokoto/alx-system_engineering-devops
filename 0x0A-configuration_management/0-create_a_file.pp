e78ede06c688a6eb3b8747fdff206031a19f0de9
LikemDzokoto <likemdzokoto@gmail.com>
2022-10-14 22:59:45 -0700

    push

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Using puppet, create a file with below specifications
+
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
